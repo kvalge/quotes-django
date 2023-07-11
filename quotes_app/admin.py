bd19dfbda669695f42630e00d35e84526602d0f7
Katrin Valge <katrin.valge@mail.ee>
2023-07-11 19:52:44 +0300

    Created admin model.

--- quotes_app/admin.py
+++ quotes_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from quotes_app.models import Quote
 
-# Register your models here.
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ("author", "quote", "years", "area", "image")
+    list_filter = ("author", "area")
+    search_fields = ("author", "years", "area")
+
+admin.site.register(Quote, QuoteAdmin)
