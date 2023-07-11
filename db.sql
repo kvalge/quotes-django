c650e1fb3ac3e823579ed87ea850d415aa75936e
Katrin Valge <katrin.valge@mail.ee>
2023-07-11 19:59:37 +0300

    Added sql script for insert data to database.

---
+++ db.sql
@@ -0,0 +1,118 @@
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Albert Einstein',
+    'Weak people revenge. Strong people forgive. Intelligent People Ignore.',
+    '1879 – 1955',
+    'Science',
+    'https://www.famousmathematicians.net/photos/albert-einstein.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Albert Einstein',
+    'I have no special talent. I am only passionately curious.',
+    '1879 – 1955',
+    'Science',
+    'https://assets.editorial.aetnd.com/uploads/2009/10/albert-einstein-gettyimages-544750041.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'John F. Kennedy',
+    'Those who dare to fail miserably can achieve greatly.',
+    '1917 – 1963',
+    'Politics',
+    'https://www.kennedy-center.org/globalassets/memorial/jfk-upward-cropped-right.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Winston Churchill',
+    'Keep calm and carry on.',
+    '1874 – 1965',
+    'Politics',
+    'https://ichef.bbci.co.uk/news/624/cpsprodpb/11B02/production/_112205427_bd7931ce-206c-49a8-82cd-6fc5efe58397.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Mark Twain',
+    'Whenever you find yourself on the side of the majority, it is time to pause and reflect.',
+    '1835 – 1910',
+    'Literature',
+    'https://cdn.britannica.com/59/96659-050-4348D4A3/Mark-Twain.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Oscar Wilde',
+    'Be yourself; everyone else is already taken.',
+    '1854 – 1900',
+    'Literature',
+    'https://cdn.britannica.com/38/215838-131-2CA17F5A/Irish-author-Oscar-Wilde-Napoleon-Sarony-photographer-1882.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Lao Tzu',
+    'The journey of a thousand miles begins with one step.',
+    'l. c. 500 BCE',
+    'Philosophy',
+    'https://wiseowlquotes.com/wp-content/uploads/2019/10/Lao-Tzu.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Wayne Gretzky',
+    'You miss 100 percent of the shots you never take.',
+    'Born in 1961',
+    'Sport',
+    'https://www.sportsnet.ca/wp-content/uploads/2022/06/CP136946875-640x360.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Steve Jobs',
+    'Stay hungry, stay foolish.',
+    '1955 – 2011',
+    'Business',
+    'https://d23.com/app/uploads/2013/08/1180w-600h_disney-legends-steven-jobs.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Steve Jobs',
+    'Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose. You are already naked. There is no reason not to follow your heart.',
+    '1955 – 2011',
+    'Business',
+    'https://addicted2success.com/wp-content/uploads/2017/11/10-Things-We-Can-Learn-From-the-Incredible-Steve-Jobs.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Frida Kahlo',
+    'Nothing is worth more than laughter. It is strength to laugh and to abandon oneself, to be light.',
+    '1907 – 1954',
+    'Art',
+    'https://www.respectability.org/wp-content/uploads/2017/10/Frida-Kahlo-2.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Maya Angelou',
+    'If you are always trying to be normal, you will never know how amazing you can be.',
+    '1928 - 2014',
+    'Literature',
+    'https://www.themarginalian.org/wp-content/uploads/2023/01/mayaangelou6.jpg?fit=1188%2C624&ssl=1');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Marie Curie',
+    'Life is not easy for any of us. But what of that? We must have perseverance and above all confidence in ourselves. We must believe that we are gifted for something and that this thing must be attained.',
+    '1867 - 1934',
+    'Science',
+    'https://hips.hearstapps.com/hmg-prod/images/gettyimages-50694623.jpg?crop=1xw:0.75xh;center,top&resize=1200:*');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Indira Gandhi',
+    'Tolerance and compassion are active, not passive states, born of the capacity to listen, to observe and to respect others.',
+    '1917 - 1984',
+    'Politics',
+    'https://images.livemint.com/img/2021/10/31/1600x900/IndiraGandhi_1551644497497_1635708119712.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Georgia O''Keeffe',
+    'I’ve been absolutely terrified every moment of my life—and I’ve never let it keep me from doing a single thing I wanted to do.',
+    '1887 - 1986',
+    'Art',
+    'https://cdn.britannica.com/19/140519-050-59D647AB/Portrait-Georgia-O-Keeffe-Alfred-Stieglitz-1922.jpg');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Yoko Ono',
+    'Some people are old at 18 and some are young at 90. Time is a concept that humans created.',
+    'Born in 1933',
+    'Art',
+    'https://cdn.britannica.com/10/150910-050-22F7AD9B/Yoko-Ono.jpg?w=400&h=300&c=crop');
+
+INSERT INTO form (id, author, quote, years, area, image)
+VALUES (DEFAULT,'Malala Yousafzai',
+    'Let us make our future now, and let us make our dreams tomorrow’s reality.',
+    'Born in 1997',
+    'Politics',
+    'https://www.un.org/sites/un2.un.org/files/styles/large-article-image-style-16-9/public/field/image/2023/01/un7952210_20220919_lf_1683_.jpg');
