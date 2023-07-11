13ed598a3276ecaa8d16ec9d6833e049c1ebb257
Katrin Valge <katrin.valge@mail.ee>
2023-07-11 19:25:07 +0300

    Created Quote model. Added database credentials to project settings.

--- quotes_app/models.py
+++ quotes_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    author = models.CharField(max_length=250, unique=False)
+    quote = models.CharField(max_length=4500, unique=False)
+    years = models.CharField(max_length=250, unique=False)
+    area = models.CharField(max_length=150, unique=False)
+    image = models.CharField(max_length=5000, unique=False)
+
+    def __str__(self):
+        return self.author
